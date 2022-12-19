8931ed4153e58b9842fbcd6b9a3e0748b80af7e7
anweress <chlayfa.anouar@gmail.com>
2022-12-20 00:50:09 +0100

    00:49 20/12/2022

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
-// const redis = require('redis');
+ const redis = require('redis');
 
 
 var express = require('express');
@@ -9,24 +9,24 @@ var app = express();
 
 // redis
 const REDIS_PORT = 6379;
-// const REDIS_HOST = 'redis';
-const REDIS_HOST = '127.0.0.1';
+const REDIS_HOST = 'redis';
+//const REDIS_HOST = '127.0.0.1';
 
-// const redisClient = redis.createClient({
-// 	url: `redis://${REDIS_HOST}:${REDIS_PORT}`
-// });
+const redisClient = redis.createClient({
+	url: `redis://${REDIS_HOST}:${REDIS_PORT}`
+});
 
-// redisClient.on('error', (err) => console.log('--- Redis Client Error', err));
-// redisClient.on('connect', () => console.log('+++ connected to Redis Client'));
-// redisClient.connect();
+redisClient.on('error', (err) => console.log('--- Redis Client Error', err));
+redisClient.on('connect', () => console.log('+++ connected to Redis Client'));
+redisClient.connect();
 
 
 
 const DB_USER = 'mongodb';
 const DB_PASSWD = 'mongodb';
 const DB_PORT = 27017;
-// const DB_HOST = 'mongo'; // default ip
-const DB_HOST = '127.0.0.1'; // default ip
+const DB_HOST = 'mongo'; // default ip
+// const DB_HOST = '127.0.0.1'; // default ip
 
 mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWD}@${DB_HOST}:${DB_PORT}`)
 	.then(() => console.log('connect to DB'))
@@ -38,10 +38,10 @@ app.get('/', function (req, res) {
 	res.send('Hello World again again WOWWOWWOW!');
 });
 
-// // test redis
-// app.get('/test-redis', async (req, res) => {
-// 	const valuePI = await redisClient.get('PI');
-// 	res.send(`\n\n###[PI] = ${valuePI}\n\n`);});
+// test redis
+app.get('/test-redis', async (req, res) => {
+	const valuePI = await redisClient.get('PI');
+	res.send(`\n\n###[PI] = ${valuePI}\n\n`);});
 
 app.listen(PORT, function () {
 	console.log('Example app li stening on port : '+ PORT);
